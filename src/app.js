2a330a28cbe7fe512a5d172ae8a32414d15c7065
Aakarsh Ranjan <aakarshranjan9@gmail.com>
2020-01-31 22:31:56 +0530

    Final

--- src/app.js
+++ src/app.js
@@ -77,6 +77,8 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen(3000,()=>{
-    console.log('Server is up')
+const port = process.env.PORT || 3000
+
+app.listen(port,()=>{
+    console.log('Server is up '+port)
 })
